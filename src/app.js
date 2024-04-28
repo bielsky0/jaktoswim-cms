225d3af81338e2df4cf6f3d856525b45e6282db4
J Bielsky <jbielsky00@gmail.com>
2024-04-28 11:00:19 +0200

    dasd

--- src/app.js
+++
@@ -1,6 +0,0 @@
-module.exports.hello = async (event, context) => {
-  return {
-    body: JSON.stringify({ message: "Hello from Serverless" }),
-    headers: { "Content-Type": "application/json" },
-  };
-};
