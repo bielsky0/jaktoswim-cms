5ab89fa4100cd6d7d788b557cdea32f8792f357b
J Bielsky <jbielsky00@gmail.com>
2024-04-28 11:41:28 +0200

    dasdas

--- config/server.js
+++ config/server.js
@@ -1,10 +1,15 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: [
+      "HlN0YVRzHSmxvuWZEZ1awg==",
+      "hVS8KkLhbuKpNiyoG0FJwg==",
+      "RpR2ZvZF6G4OQry9bUXo9Q==",
+      "SsR+DkVkMF0HmkSFygafoQ==",
+    ],
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
